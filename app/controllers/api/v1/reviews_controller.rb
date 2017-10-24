62ab57b5908812808901bab54876fa0a4c6a378e
Catherine <cmlavin7@gmail.com>
2017-10-24 15:14:23 -0400

    Changes to reviews controller

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,40 @@
 class Api::V1::ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :rating, :created_at, :edited_at)
+  end
+
 end
